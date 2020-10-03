60401f9678c7d27608f0445ab378a2d1ffd034d3
Basile Bruneau <basilebruneau@gmail.com>
2020-10-03 12:07:10 +0200

    feat: add addOutlineString

--- test.js
+++ test.js
@@ -18,5 +18,7 @@ joto.addFAIcon({ x: 140, y: 120, size: 80, icon: faCloudRain });
 joto.addFAIcon({ x: 270, y: 120, size: 80, icon: faCloudSun });
 joto.addFAIcon({ x: 400, y: 120, size: 80, icon: faCloudMoon });
 
+joto.addOutlineString({ x: 60, y: 250, size: 40, str: '73 401', fontFilePath: './font.ttf' });
+
 const svg = joto.getSVG();
 fs.writeFileSync('./joto.svg', svg, { encoding: 'utf8' });
